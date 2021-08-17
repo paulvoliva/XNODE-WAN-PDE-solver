0cdfb20dcff66214512244aedf110ebe745d7caa
Cuiyu <45636707+cuiyu-he@users.noreply.github.com>
2021-08-17 13:19:23 -0400

    Update training.py

--- NODE GAN/training.py
+++ NODE GAN/training.py
@@ -15,9 +15,9 @@ def func_eval(X: torch.Tensor, BX: torch.Tensor, setup: dict, y_output_u: torch.
 
     Args:
         X: interior data points
-        BX: border data points
-        y_output_u: our guess for u
-        func_: all the funcs that are the coefficients and boundary values from the PDE
+        BX: boundary data points
+        y_output_u: our guess for u  #He. I recomend to change y_output_u -> pred_u.
+        func_: all the given functions to the PDE
     '''
 
     h = func_h(X[:, 0, :])
@@ -26,13 +26,13 @@ def func_eval(X: torch.Tensor, BX: torch.Tensor, setup: dict, y_output_u: torch.
 
     c = func_c(X, y_output_u)
 
-    # this is meant to be a d by d-dimensional array containing domain_sample_size by 1 tensors
+    # a is meant to be a d by d-dimensional array with each element containing a tensor of size = [X.shape[0], X.shape[1]].
     a = torch.Tensor(setup['dim'], setup['dim'], X.shape[0], X.shape[1])
 
     for i, j in product(range(setup['dim']), repeat=2):
         a[i, j] = func_a(X, i, j)
 
-    # this is meant to be a d-dimensional containing domain_sample_size by 1 tensors
+    # b is meant to be a d-dimensional array with each element containing a tensor of size = [X.shape[0], X.shape[1]].
     b = torch.Tensor(setup['dim'], X.shape[0], X.shape[1])
 
     for i in range(setup['dim']):
@@ -42,16 +42,16 @@ def func_eval(X: torch.Tensor, BX: torch.Tensor, setup: dict, y_output_u: torch.
 
 
 def init_weights(layer):
+    #He. this function ensures the correct initialization
     if type(layer) == torch.nn.Linear:
         torch.nn.init.xavier_uniform_(layer.weight)
         layer.bias.data.fill_(0)
 
-# this function ensures that the layers have the right weight initialisation
-
-
+ 
+#He. I suggest change NODE_solver -> XNODE_WAN_solvre
 class NODE_solver:
     '''
-    The WAN algorithm runs in this class
+    this class runs the XNODE_WAN algorithm
 
     Args:
         params: dict: a dictionary with our setup and config dicts as well as the number of iterations
@@ -60,7 +60,7 @@ class NODE_solver:
         func_u_sol: the solution to our PDE if known
         p: if fun_u_sol is given the norm difference of our guess to the true solution in L^p is computed
     '''
-    def __init__(self, params: dict, func_a, func_b, func_c, func_h, func_f, func_g, domain, func_u_sol=None, p: float = 1):
+    def __init__(self, params: dict, func_a, func_b, func_c, func_h, func_f, func_g, domain, func_u_sol = None, p: float = 1):
         self.params = params
         self.func_a = func_a
         self.func_b = func_b
@@ -79,18 +79,21 @@ class NODE_solver:
         self.n1 = self.config['n1']
         self.n2 = self.config['n2']
 
-        self.points = Comb_loader(self.setup['N_r'], self.setup['N_b'], self.domain)
+        self.points = Comb_loader(self.setup['N_r'], self.setup['N_b'], self.domain) #He. I don't think this line is needed here.
 
-        # neural network models
+        # seu up neural network models
+        #He. I suugest change NeuralODE->XNODE and discriminator -> DNN_v (we did not mention discriminator in our paper).
         self.u_net = NeuralODE(self.config['u_hidden_dim'], 1, self.func_h, self.setup,
                                self.config['u_hidden_hidden_dim'], self.config['u_layers'], self.config['solver'],
                                self.config['min_steps'], self.config['adjoint']).to(device) #torch.nn.DataParallel().to(device)
+        
         self.v_net = discriminator(self.config, self.setup).to(device) #torch.nn.DataParallel().to(device)
-
+        
+        #initialization
         self.u_net.apply(init_weights)
         self.v_net.apply(init_weights)
 
-        # optimizers for WAN
+        # set up optimizers for v_net and u_net
         self.optimizer_u = torch.optim.Adam(self.u_net.parameters(), lr=self.config['u_rate'])
         self.optimizer_v = torch.optim.Adam(self.v_net.parameters(), lr=self.config['v_rate'])
 
@@ -102,7 +105,7 @@ class NODE_solver:
             show_plt: whether or not to show a plot
         '''
         for k in range(self.iterations):
-            points = Comb_loader(self.setup['N_r'], self.setup['N_b'], self.domain)
+            points = Comb_loader(self.setup['N_r'], self.setup['N_b'], self.domain) #He. what does Comb mean here?
 
             for i in range(self.n1):
                  for (datau, datav, bdata) in points:
@@ -114,7 +117,7 @@ class NODE_solver:
                                                  self.func_c, self.func_h, self.func_f, self.func_g)
                     Loss = loss(self.config['alpha'], a, b, c, h, f, g, self.setup, self.domain)
                     loss_u = Loss.u(prediction_u, prediction_v, self.u_net, datau, datav, bdata)
-                    loss_u.backward(retain_graph=True)
+                    loss_u.backward(retain_graph = True)
                     self.optimizer_u.step()
 
             for j in range(self.n2):
