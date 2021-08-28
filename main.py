37c04bdc4f8ec117879807a9481586bc1737bcd9
paulvoliva <76442605+paulvoliva@users.noreply.github.com>
2021-08-28 14:04:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.training import NODE_WAN_solver
 import torch
+import argparse
+import yaml
 from src.dataset import *
+import importlib
 
 '''
 This is the document that allows to solve the PDEs and to interact with the algorithm.
@@ -18,83 +21,33 @@ u(x, 0)=h(x), & \text { in } \Omega
 where $x$ is a d-dimensional vector. You specify these functions just below
 '''
 
+parser = argparse.ArgumentParser(prog='XNODE-WAN PDE solver',
+                                 description='a general purpose parabolic PDE solver using the XNODE-WAN architecture')
+# TODO: implement directory
+parser.add_argument('-w', '--work_dir', type=str, default='./', help='directory for the best model parameters')
+
+parser.add_argument('--params', help='an experiment setup to load', required=True)
+parser.add_argument('--funcs', help='location of the functions for the PDE (omit .py)', required=True)
+parser.add_argument('--device', default=None, help='the device to run the experiment, default is cuda if available')
+parser.add_argument('--report', type=bool, default=True, help='boolean for reporting the progress')
+parser.add_argument('--report_it', type=int, default=10, help='number of iterations between reporting progress')
+parser.add_argument('--show_plt', type=bool, default=False, help='whether to show plots at report_it intervals')
+
+args = parser.parse_args(['--params', 'cube_pde.yaml', '--funcs', 'cube_pde_funcs'])
+#
+#__import__('configs.' + args.funcs)
+funcs = importlib.import_module('configs.'+args.funcs)
+names = [x for x in funcs.__dict__ if not x.startswith("_")]
+globals().update({k: getattr(funcs, k) for k in names[2:]})
+
+with open('NODE_GAN/configs/'+args.params, 'r') as parameters:
+    params = yaml.safe_load(parameters)
+
 # setting to cuda
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-''' # Setting Parameters '''
-
-# dictionary with all the configurations of meshes and the problem dimension
-
-setup = {
-    'dim': 5,       # int: dimension of the problem
-    'N_t': 20,      # int: number of time samples
-    'N_r': 400,     # int: number of spatial samples in the interior
-    'N_b': 400,     # int: number of spatial samples on the boundary
-    'T0': 0,        # float: initial time
-    'T': 1,          # float: final time
-    'shape_param': 1
-}
-
-# hyperparameters
-
-config = {
-    'alpha': 1e4 * 400 * 25,    # float: the coefficient in our loss function
-    'u_layers': 8,              # int: depth of the hidden field F
-    'u_hidden_dim': 20,         # int: dimensionality of the initial and final layers
-    'u_hidden_hidden_dim': 10,  # int: dimensionality of the hidden field F
-    'v_layers': 9,              # int: the depth of the adversarial DNN
-    'v_hidden_dim': 50,         # int: the dimensionality of the adversarial DNN
-    'n1': 2,                    # int: sub-iterations for fitting of the guess
-    'n2': 1,                    # int: sub-iterations for fitting of the test function
-    'u_rate': 0.015,            # float: learning rate of the guess
-    'v_rate': 0.04,             # float: learning rate of the test function
-    'min_steps': 5,             # int: smallest number of time steps used in the ODE solver
-    'adjoint': False,           # bool: whether to use an adjoint solver for the ODE solver
-    'solver': 'midpoint'        # str: the solver to be used in the ODE solver
-}
-
-iterations = 2e4+1
-
-
-'''
-# Setting the specific problem to solve
-'''
-
-
-def func_u_sol(X):
-    return X[:, :, 0]
-
-
-def func_f(X):
-    return X[:, :, 0]
-
-
-def func_g(BX):
-    return BX[:, :, 0]
-
-
-def func_h(X):
-    return X[:, 0]
-
-
-def func_a(X, i, j):
-    return X[:, :, 0]
-
-
-def func_b(X, i):
-    return X[:, :, 0]
-
-
-# the following function can take into account the function u, so they have the input `y_output_u` which will be our
-# guess solution
-
-def func_c(X, y_output_u):
-    return X[:, :, 0]
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') if args.device is None else args.device
 
 
 if __name__ == '__main__':
-    params = {**config, **setup, **{'iterations': int(iterations)}}
-    solver = NODE_WAN_solver(params, func_a, func_b, func_c, func_h, func_f, func_g, NSphere_TCone)
-    solver.train(report=True)
+    solver = NODE_WAN_solver(params, func_a, func_b, func_c, func_h, func_f, func_g, device, func_u_sol=func_u_sol)
+    solver.train(report=args.report, report_it=args.report_it, show_plt=args.show_plt)
